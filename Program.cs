3611186dbb75977d7be63d0363d2d5f7e98fba2b
lilyqin7 <lilyqin527@gmail.com>
2026-03-12 22:45:31 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.IO;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("Enter a GitHub username: ");
+        string username = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(username))
+        {
+            Console.WriteLine("Invalid username.");
+            return;
+        }
+    }
+}
